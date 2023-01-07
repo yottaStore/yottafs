77deba3fc1881be6fbe1dd70de2dc7213ef75f45
Alberto <alberto.websci@gmail.com>
2023-01-07 14:08:20 +0100

    feat(config): json parsing (#1)
    * feat: single device config
    
    * feat(config): arrays of devices

---
+++ packages/config/src/config.rs
@@ -0,0 +1,25 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Debug, Deserialize)]
+pub struct Device {
+    pub path: String,
+    pub start_sector: Option<u64>,
+    pub end_sector: Option<u64>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub hostname: String,
+    pub devices: Vec<Device>,
+}
+
+pub fn parse_config(path: &str) -> Result<Config, Box<dyn Error>> {
+    let json_file_path = Path::new(path);
+    let file = File::open(json_file_path)?;
+    let config: Config = serde_json::from_reader(file)?;
+
+    return Ok(config);
+}
---
+++ packages/config/src/main.rs
@@ -0,0 +1,12 @@
+use config::config::parse_config;
+use std::env;
+
+fn main() {
+    println!("Hello, world!");
+    //println!("Current dir: {:?}", env::current_dir());
+
+    let file_path = "./e2e/config.json";
+    let c = parse_config(file_path).unwrap();
+
+    println!("config: {:?}", c);
+}
